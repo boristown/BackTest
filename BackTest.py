e87defe6c41f8542ba20c94561c63b5ce4721788
boristown <boristown@gmail.com>
2019-08-28 14:23:35 +0800

    迭代1：连接数据库

--- BackTest.py
+++ BackTest.py
@@ -1,1 +1,24 @@
+import pymssql
+import datetime
+import mypsw
 
+print(datetime.datetime.now(), "连接中……")
+
+conn = pymssql.connect(
+    host=mypsw.host, 
+    user=mypsw.user,
+    password=mypsw.password,
+    database=mypsw.database)
+
+print(datetime.datetime.now(), "读取数据中……")
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT TOP (10) [ID] ,[Symbol] from [Investingcom]")
+
+records = cursor.fetchall()
+
+print(datetime.datetime.now(), "读取到", len(records), "条记录:")
+
+for row in records:
+    print(datetime.datetime.now(), row)
