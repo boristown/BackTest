443ae425ca941e772455db4d11b8239d1412b98d
boristown <boristown@gmail.com>
2019-08-29 12:10:29 +0800

    迭代4：生成价格清单

--- BackTest.py
+++ BackTest.py
@@ -23,7 +23,34 @@ cursor.execute(
 
 records = cursor.fetchall()
 
-#for row in records:
+price_count = 120
+current_row_index = 0
+
+for row in records:
+    price_list = []
+    for price_index in range(price_count):
+        last_row_index = current_row_index + price_index
+        if row[last_row_index][1] == row[current_row_index][1]:
+            price_list.append(row[last_row_index][3])
+        else:
+            break
+    if len(price_list) == price_count:
+        max_price = max(price_list)
+        min_price = min(price_list)
+        center_price = (max_price + min_price) / 2
+        range_price = max_price - min_price
+        if range_price > 0:
+            for price_index in range(price_count):
+                price_list[price_index] -= center_price
+                price_list[price_index] /= range_price
+                price_list[price_index] += 0.5
+                if price_list[price_index] > 0.99999:
+                    price_list[price_index] = 1.0
+                elif price_list[price_index] < 0.00001:
+                    price_list[price_index] = 0.0
+
+    current_row_index += 1
+
 #    print(datetime.datetime.now(), row)
 
 print(datetime.datetime.now(), "读取到", len(records), "条记录:")
